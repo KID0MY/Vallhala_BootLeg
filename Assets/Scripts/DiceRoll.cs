a3b26f8bbbed09e6e68761cc7532ef9f95e3d07f
Lucas Triboli Bujes <57596595+KID0MY@users.noreply.github.com>
2024-09-21 12:32:36 -0400

    adding comments
    Just added some comements to help out  Zach to understand what I have already done. Hope it helps!

--- Assets/Scripts/DiceRoll.cs
+++ Assets/Scripts/DiceRoll.cs
@@ -2,17 +2,22 @@ using UnityEngine;
 
 public class DiceRoll : MonoBehaviour
 {
+    //Variables to make the dice Movement
     public int oneD6;
     public bool DiceRolled;
-    int i = -1;
+   
+    
+    int i = -1; // the int i is just a index pointer to find the players
 
     // Update is called once per frame
     void Update()
     {
     }
 
+    // Function to make the dice roll
     public void RollDice()
     {
+        //Checking if i is bigger then the player count and if i is smaller then i 
         if (i < PlayerMovement.players.Count && i < 3)
         {
             i++;
@@ -21,6 +26,8 @@ public class DiceRoll : MonoBehaviour
         {
             i = 0;
         }
+
+        // Run a randomizer from 1 to 6 and then add the roll to the players movement
         oneD6 = UnityEngine.Random.Range(1, 7);
         PlayerMovement.players[i].PlayerMove();
         //print(oneD6);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    // Calling the DiceRoll Script 
     public DiceRoll script;
+
+    //Making the player list
     public static List<PlayerMovement> players = new List<PlayerMovement>();
+    
+    //Storing which tile the player is and how many points they have 
     public int playersTile;
     public int PlayerPoints;
-    // Start is called before the first frame update
 
+    //Awake is called when the script object is initialised, regardless of whether or not the script is enabled. 
     void Awake()
     {
+        //add player to list
         players.Add(this);
     }
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Check if player passed the 12 tile if so return him to base and add poitns
         if (playersTile >= 12)
         {
             PlayerPoints += playersTile - 12;
@@ -24,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    //Player Movement(transform its position and add the ammount of tiles to the variable)
     public void PlayerMove()
     {
         transform.position += Vector3.right * script.oneD6;
